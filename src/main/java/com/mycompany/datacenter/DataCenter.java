f20bdbb2ba5323ec5c47f99add60254d4247aa10
RyanMashaira <ST10535572@myemeris.edu.za>
2026-04-09 18:57:48 +0200

    Array list created
    added the array list for storing the user credentials

--- src/main/java/com/mycompany/datacenter/DataCenter.java
+++ src/main/java/com/mycompany/datacenter/DataCenter.java
@@ -1,16 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
+// imports declaration 
 package com.mycompany.datacenter;
+// imports declaration 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
-/**
- *
- * @author masha
- */
 public class DataCenter {
+    
+    // creating the user list array list 
+    static ArrayList<String> usernames = new ArrayList<>();
+    static ArrayList<String> pins = new ArrayList<>();
+    static ArrayList<Integer>roles = new ArrayList<>();   
+    static ArrayList<String> createdAt = new ArrayList<>();
+    static ArrayList<ArrayList<String>> loginLogs = new ArrayList<>(); 
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 }
