2e5af94fdce35a48c6b9623f7eb522fd8461b337
RyanMashaira <ST10535572@myemeris.edu.za>
2026-04-09 18:50:31 +0200

    file creation

---
+++ src/main/java/com/mycompany/datacenter/DataCenter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datacenter;
+
+/**
+ *
+ * @author masha
+ */
+public class DataCenter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
