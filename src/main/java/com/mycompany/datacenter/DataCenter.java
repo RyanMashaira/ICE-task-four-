9a45f067265ff6bbaaeea5d46b7d06bffb90390d
RyanMashaira <ST10535572@myemeris.edu.za>
2026-04-09 19:05:05 +0200

    Adding lock out tracking for all users
    created a ways to make sure that the users have a finite number of tries before being locked out

--- src/main/java/com/mycompany/datacenter/DataCenter.java
+++ src/main/java/com/mycompany/datacenter/DataCenter.java
@@ -16,5 +16,16 @@ public class DataCenter {
     static ArrayList<Integer>roles = new ArrayList<>();   
     static ArrayList<String> createdAt = new ArrayList<>();
     static ArrayList<ArrayList<String>> loginLogs = new ArrayList<>(); 
-
+    
+    // Lockout tracking for the arrays 
+    static ArrayList<Boolean> lockedOut = new ArrayList<>();
+    static ArrayList<Integer> failedAttempts  = new ArrayList<>();
+    
+    static final int Max_Attempt = 3; // setting a static or contast number of tries for the users 
+    static ArrayList<Integer> failedAttempts  = new ArrayList<>();
+    
+    static Scanner scanner = new Scanner(System.in); // creation of a scanner to accept the data 
+    static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    
+    
 }
